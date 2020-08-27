a52e9b6393a14d919d8ee0d83d8d3d00196d3af3
dot4qu <dot4qu@virginia.edu>
2020-08-26 17:05:45 -0700

    Dead simple impl of https running on 9443 in conjunction with http on 9080

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
-// import https from "https";
+import https from "https";
 import http from "http";
+import fs from "fs";
 
 import router from "./routes";
 import { twoDigits } from "./helpers";
@@ -17,5 +18,33 @@ app.use((req: express.Request, res: express.Response, next: express.NextFunction
 app.use(bodyParser.json());
 app.use(router());
 
+// HTTPS certs
+let creds = {};
+if (process.env.DEPLOY_STAGE === "PROD") {
+    if (!process.env.PROD_SSL_KEY_PATH || !process.env.PROD_SSL_CERT_PATH || !process.env.PROD_SSL_CA_CERT_PATH) {
+        console.log("SSL cert env variables not set. Source the setup_env.sh script");
+        process.exit(1);
+    }
+
+    const key = fs.readFileSync(process.env.PROD_SSL_KEY_PATH);
+    const cert = fs.readFileSync(process.env.PROD_SSL_CERT_PATH);
+    const ca = fs.readFileSync(process.env.PROD_SSL_CA_CERT_PATH);
+    creds = {
+        key,
+        cert,
+        ca
+    };
+} else {
+    console.log("Running server locally using local self-signed cert");
+    const localKey = fs.readFileSync(__dirname + "/../spotcheck-selfsigned-key.pem", "utf-8");
+    const localCert = fs.readFileSync(__dirname + "/../spotcheck-selfsigned-cert.pem", "utf-8");
+    creds = {
+        key: localKey,
+        cert: localCert
+    };
+}
+
+const httpsServer = https.createServer(creds, app);
 const httpServer = http.createServer(app);
+httpsServer.listen(9443);
 httpServer.listen(9080);
